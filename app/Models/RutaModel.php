e8e2067a5244c8b81c43df378724111e86291c65
Danang Wisnu Prabowo <222111975@stis.ac.id>
2023-12-10 15:27:33 +0700

    ruta model dan libraries

--- app/Models/RutaModel.php
+++ app/Models/RutaModel.php
@@ -7,7 +7,7 @@ use CodeIgniter\Model;
 
 class RutaModel extends Model
 {
-    protected $table            = 'rutas';
+    protected $table            = 'rumahtangga';
     protected $primaryKey       = 'kodeRuta';
     // protected $useAutoIncrement = true;
     // protected $returnType       = 'array';
@@ -28,5 +28,16 @@ class RutaModel extends Model
     {
         $ruta = (array) $ruta;
         return $this->replace($ruta);
+    }
+
+    public function updateRuta(Rumahtangga $ruta): bool
+    {
+        $ruta = (array) $ruta;
+        return $this->replace((array) $ruta);
+    }
+
+    public function deleteRuta(Rumahtangga $ruta): bool
+    {
+        return $this->delete(['kodeRuta' => $ruta->kodeRuta]);
     }
 }
