c4005250b98291780ab58f84ee9f74eae216134d
alfiansyrff <alfianfurqon27@gmail.com>
2023-12-10 15:44:33 +0700

    feat: Listing sync ruta pcl

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -8,4 +8,6 @@ use CodeIgniter\Router\RouteCollection;
 $routes->get('/', 'Home::index');
 
 $routes->get('login', 'LoginController::index');
+$routes->post('listing', 'ListingController::index');
+
 $routes->get('check-danang', 'WilayahKerjaController::index');
