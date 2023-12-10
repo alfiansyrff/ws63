75bf4276fd317b252600ab635095528a546bc7a6
Imalia Rosyida <imaliarosidah@gmail.com>
2023-12-10 19:05:23 +0700

    refactor: buat response posisipcl

--- app/Controllers/PosisiPclController.php
+++ app/Controllers/PosisiPclController.php
@@ -13,26 +13,38 @@ class PosisiPclController extends BaseController
     use ResponseTrait;
     public function index()
     {
-        $PosisiPclModel = new PosisiPclModel();
+        if (isset($_POST['nim'])) {
+            $PosisiPclModel = new PosisiPclModel();
+            $nim = (string) $this->request->getPost('nim');
+            $object = $PosisiPclModel->getPosisiPcl($nim);
 
-        $object = $PosisiPclModel->getPosisiPcl("222112975");
-
-        return $this->respond($object, 200);
+            return $this->respond($object, 200);
+        } else {
+            return $this->failNotFound('NIM tidak ditemukan');
+        }
     }
 
     public function updateLokasiPcl(){
+        
         if (isset($_POST['nim'])) {
             $model = new PosisiPclModel();
 			
-        
             $nim = (string) $this->request->getPost('nim');
             $latitude = (float) $this->request->getPost('latitude');
             $longitude = (float) $this->request->getPost('longitude');
             $lokus = (string) $this->request->getPost('lokus');
 
+            $existingData = $model->find($nim);
+
+        if (!$existingData) {
+            // Jika NIM tidak ditemukan, kembalikan respons 404
+            return $this->failNotFound('NIM tidak ditemukan');
+        }
+
             if ($model->updateLokasiPcl($nim, $latitude, $longitude, $lokus)) {
                 return $this->respond("Lokasi berhasil diupdate.", 200);
             }
         }
+        return $this->fail("Internal server error.");
     }
 }
