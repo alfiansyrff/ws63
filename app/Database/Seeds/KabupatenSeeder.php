d1209aecafafefaf4930a3466d4b7ddd48c5a765
alfiansyrff <alfianfurqon27@gmail.com>
2024-01-19 18:49:37 +0700

    Add seeder

--- app/Database/Seeds/KabupatenSeeder.php
+++ app/Database/Seeds/KabupatenSeeder.php
@@ -17,6 +17,23 @@ class KabupatenSeeder extends Seeder
                 'id_kab'   => '002',
                 'nama_kab' => 'Gianyar',
             ],
+            [
+                'id_kab' => '06',
+                'nama_kab' => 'Bangli'
+            ],
+            [
+                'id_kab' => '07',
+                'nama_kab' => 'Karangasem'
+            ],
+            [
+                'id_kab' => '08',
+                'nama_kab' => 'Buleleng'
+            ],
+            [
+                'id_kab' => '71',
+                'nama_kab' => 'Denpasar'
+            ],
+            
         ];
 
         // Insert data ke dalam tabel
--- app/Database/Seeds/KecamatanSeeder.php
+++ app/Database/Seeds/KecamatanSeeder.php
@@ -10,15 +10,131 @@ class KecamatanSeeder extends Seeder
     {
         $data = [
             [
-                'id_kab' => '001',
-                'id_kec' => '001',
-                'nama_kec' => 'Kecamatan A',
+                'id_kab' => '71',
+                'id_kec' => '010',
+                'nama_kec' => 'Denpasar Selatan',
             ],
             [
-                'id_kab' => '001',
-                'id_kec' => '002',
-                'nama_kec' => 'Kecamatan B',
+                'id_kab' => '71',
+                'id_kec' => '020',
+                'nama_kec' => 'Denpasar Timur',
             ],
+            [
+                'id_kab' => '71',
+                'id_kec' => '030',
+                'nama_kec' => 'Denpasar Barat',
+            ],
+            [
+                'id_kab' => '71',
+                'id_kec' => '031',
+                'nama_kec' => 'Denpasar Utara',
+            ],
+            [
+                'id_kab' => '06',
+                'id_kec' => '010',
+                'nama_kec' => 'Susut',
+            ],
+            [
+                'id_kab' => '06',
+                'id_kec' => '020',
+                'nama_kec' => 'Bangli',
+            ],
+            [
+                'id_kab' => '06',
+                'id_kec' => '030',
+                'nama_kec' => 'Tembuku',
+            ],
+            [
+                'id_kab' => '06',
+                'id_kec' => '040',
+                'nama_kec' => 'Kintamani',
+            ],
+            [
+                'id_kab' => '07',
+                'id_kec' => '010',
+                'nama_kec' => 'Rendang',
+            ],
+            [
+                'id_kab' => '07',
+                'id_kec' => '020',
+                'nama_kec' => 'Sidemen',
+            ],
+            [
+                'id_kab' => '07',
+                'id_kec' => '030',
+                'nama_kec' => 'Manggis',
+            ],
+            [
+                'id_kab' => '07',
+                'id_kec' => '040',
+                'nama_kec' => 'Karangasem',
+            ],
+            [
+                'id_kab' => '07',
+                'id_kec' => '050',
+                'nama_kec' => 'Abang',
+            ],
+            [
+                'id_kab' => '07',
+                'id_kec' => '060',
+                'nama_kec' => 'Bebandem',
+            ],
+            [
+                'id_kab' => '07',
+                'id_kec' => '070',
+                'nama_kec' => 'Selat',
+            ],
+            [
+                'id_kab' => '07',
+                'id_kec' => '080',
+                'nama_kec' => 'Kubu',
+            ],
+            [
+                'id_kab' => '08',
+                'id_kec' => '010',
+                'nama_kec' => 'Gerokgak',
+            ],
+            [
+                'id_kab' => '08',
+                'id_kec' => '020',
+                'nama_kec' => 'Seririt',
+            ],
+            [
+                'id_kab' => '08',
+                'id_kec' => '030',
+                'nama_kec' => 'Busungbiu',
+            ],
+            [
+                'id_kab' => '08',
+                'id_kec' => '040',
+                'nama_kec' => 'Banjar',
+            ],
+            [
+                'id_kab' => '08',
+                'id_kec' => '050',
+                'nama_kec' => 'Sukasada',
+            ],
+            [
+                'id_kab' => '08',
+                'id_kec' => '060',
+                'nama_kec' => 'Buleleng',
+            ],
+            [
+                'id_kab' => '08',
+                'id_kec' => '070',
+                'nama_kec' => 'Sawan',
+            ],
+            [
+                'id_kab' => '08',
+                'id_kec' => '080',
+                'nama_kec' => 'Kubutambahan',
+            ],
+            [
+                'id_kab' => '08',
+                'id_kec' => '090',
+                'nama_kec' => 'Tejakula',
+            ],
+            
         ];
         $this->db->table('kecamatan')->insertBatch($data);
     }
